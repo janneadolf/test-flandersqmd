b8a7bf4679acb128428ccc00b6ce2b8b5c4e8dca
Janne Adolf <janne.adolf@inbo.be>
2025-06-04 12:11:52 +0200

    update script to test package

--- source/test_pkg/test_pkg.R
+++ source/test_pkg/test_pkg.R
@@ -1,9 +1,12 @@
 #pak::pkg_install("inbo/flandersqmd@draft")
-path <- "source"
-shortname <- "test_pkg"
+path <- "source/test_pkg"
+shortname <- "test_pkg_skeleton"
+# ?flandersqmd::create_report
+# "When path is a subfolder of a git repository, it is changed to the root of that git repository."
+
 # to create a report skeleton
 flandersqmd::create_report(path = path, shortname = shortname, version = "gha")
 # to run when you have the report numbers
-insert_missing_metadata(file.path(path, shortname))
+flandersqmd::insert_missing_metadata(file.path(path, shortname))
 # to run when you have the cover page
 report_path <- file.path(path, shortname)
