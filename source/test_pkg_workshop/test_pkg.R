dd4fb4d4e75911e63cfba3a9dd8a3e1c5783e9e4
Janne Adolf <janne.adolf@inbo.be>
2025-06-25 11:44:12 +0200

    test stable package version in workshop

---
+++ source/test_pkg_workshop/test_pkg.R
@@ -0,0 +1,16 @@
+# install dev version
+pak::pkg_install("inbo/flandersqmd@draft")
+
+# install stable version
+install.packages(
+  "flandersqmd",
+  repos = c("https://inbo.r-universe.dev", "https://cloud.r-project.org"))
+
+path <- "source/test_pkg_workshop"
+shortname <- "test_package_skeleton"
+# to create a report skeleton
+flandersqmd::create_report(path = path, shortname = shortname, version = "gha")
+# to run when you have the report numbers
+flandersqmd::insert_missing_metadata(file.path(path, shortname))
+# to run when you have the cover page
+report_path <- file.path(path, shortname)
