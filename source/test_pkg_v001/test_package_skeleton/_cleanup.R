505b858f04749e838865471ce745f2e1fdf147c7
Janne Adolf <janne.adolf@inbo.be>
2025-11-18 11:11:50 +0100

    add script to clean up project dir

---
+++ source/test_pkg_v001/test_package_skeleton/_cleanup.R
@@ -0,0 +1,8 @@
+list.files(
+  path = "source/test_pkg_v001/test_package_skeleton",
+  pattern = "\\.png|\\.jpg|\\.eps|\\.pdf|\\.sty|\\.tex|site_libs",
+  full.names = TRUE
+) |>
+  unlink(x = _, recursive = TRUE)
+
+
